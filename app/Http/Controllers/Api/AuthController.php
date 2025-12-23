14ac6009dc3fc1fd36aec9c6a79a58fc01c3aa1c
Mayuresh-conexus <mayuresh.mhatre@conexus-ns.com>
2025-12-23 16:42:20 +0530

    auth api quick fix 2

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -46,7 +46,7 @@ class AuthController extends Controller
             'id' => $user->id,
             'name' => $user->name,
             'email' => $user->email,
-            'roles' => $user->getRoleNames(),
+            'roles' => "volunteer",
         ]);
     }
 

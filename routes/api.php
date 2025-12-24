3ca14689bc352b5042daf4490ec6dce11ac4e697
Mayuresh-conexus <mayuresh.mhatre@conexus-ns.com>
2025-12-24 17:54:42 +0530

    improve program and project

--- routes/api.php
+++ routes/api.php
@@ -9,19 +9,15 @@ use App\Http\Controllers\Api\ProjectController;
 Route::prefix('v1')->group(function () {
     Route::post('/auth/login', [AuthController::class, 'login']);
 
-    Route::apiResource('programs', ProgramController::class);
-     Route::apiResource('projects', ProjectController::class);
-
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/auth/me', [AuthController::class, 'me']);
         Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-        
-    Route::get('/countries', [LocationController::class, 'countries']);
-    Route::get('/states', [LocationController::class, 'states']); // ?countryid=1
-    Route::get('/cities', [LocationController::class, 'cities']); // ?stateid=1
+        Route::apiResource('programs', ProgramController::class);
+        Route::apiResource('projects', ProjectController::class);
 
-    
-   
+        Route::get('/countries', [LocationController::class, 'countries']);
+        Route::get('/states', [LocationController::class, 'states']);
+        Route::get('/cities', [LocationController::class, 'cities']);
     });
 });

6003abcc90087dee3fd1bac0717466081817c047
Mayuresh-conexus <mayuresh.mhatre@conexus-ns.com>
2026-01-12 18:05:27 +0530

    Program is active only API v10

--- app/Http/Controllers/Api/ProgramController.php
+++ app/Http/Controllers/Api/ProgramController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use Illuminate\Support\Facades\Log;
 
 use App\Http\Controllers\Controller;
 use App\Models\Program;
@@ -8,21 +9,30 @@ use Illuminate\Http\Request;
 
 class ProgramController extends Controller
 {
+    
     public function list()
-    {
-        $programs = Program::query()
-    ->select('id', 'name', 'is_active')
-    ->where('is_active', 1)
-    ->orderBy('name')
-    ->get();
+{
+    $q = Program::query()
+        ->select('id', 'name', 'is_active')
+        ->where('is_active', 1)
+        ->orderBy('name');
+
+    Log::info('Programs SQL', [
+        'sql' => $q->toSql(),
+        'bindings' => $q->getBindings(),
+    ]);
+
+    $programs = $q->get();
+
+    Log::info('Programs result', $programs->toArray());
+
+    return response()->json([
+        'success' => true,
+        'data' => $programs,
+    ]);
+}
 
 
-
-        return response()->json([
-            'success' => true,
-            'data' => $programs,
-        ]);
-    }
     public function index()
     {
         return response()->json([

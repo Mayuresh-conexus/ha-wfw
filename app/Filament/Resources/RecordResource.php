d57f8c3304a17a169a021179555fd620fb64881c
Mayuresh-conexus <mayuresh.mhatre@conexus-ns.com>
2026-01-12 16:40:59 +0530

    signature path v4

--- app/Filament/Resources/RecordResource.php
+++ app/Filament/Resources/RecordResource.php
@@ -118,7 +118,7 @@ class RecordResource extends Resource
             Forms\Components\FileUpload::make('attachments')
                 ->label('Attachments')
                 ->disk('public')
-                ->directory(fn (callable $get) => 'patients/' . $get('filenumber') . '/signature')
+                ->directory(fn (callable $get) => 'patients/' . $get('patientid') . '/signature')
                 ->preserveFilenames()
                 ->multiple()
                 ->reorderable()

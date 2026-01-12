fe11665c7172dc35227c8598104c5d9feadb25ec
Mayuresh-conexus <mayuresh.mhatre@conexus-ns.com>
2026-01-12 17:40:08 +0530

    Program is active only API v2

--- app/Http/Controllers/Api/ProgramController.php
+++ app/Http/Controllers/Api/ProgramController.php
@@ -12,7 +12,7 @@ class ProgramController extends Controller
     {
         $programs = Program::query()
             ->select('id', 'name')
-            ->where('is_active', 1)
+            ->where('is_active', true)
             ->orderBy('name')
             ->get();
 

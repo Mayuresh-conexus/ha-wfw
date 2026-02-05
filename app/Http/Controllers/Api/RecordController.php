1b2ab65402d6fcca5e9eb2857d47d84392d29ea6
Mayuresh-conexus <mayuresh.mhatre@conexus-ns.com>
2026-02-05 12:33:33 +0530

    medicine api fix revert v3

--- app/Http/Controllers/Api/RecordController.php
+++ app/Http/Controllers/Api/RecordController.php
@@ -35,7 +35,7 @@ class RecordController extends Controller
             'gpid'             => 'nullable|array',
             'gpid.*'           => 'integer|exists:users,id',
             'medicineid'       => 'nullable|array',
-            'medicineid.*'     => 'string|exists:medicines,id',
+            'medicineid.*'     => 'string|max:255',
         ]);
 
         // Decode question_summary (since we can't cast LONGTEXT automatically to array easily)
